c4f28499ee9d1f81f8ecc5bf8b41032fc3f6106a
ujjwal-singh-sg <ujjwal.singh@studiographene.com>
2024-04-09 03:33:52 +0530

    This is url change in abc file.

--- Scripts/abc/Script1712576282098.groovy
+++ Scripts/abc/Script1712576282098.groovy
@@ -19,7 +19,7 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
 
-WebUI.navigateToUrl('https://www.google.com')
+WebUI.navigateToUrl('https://www.flipkart.com')
 
 WebUI.closeBrowser()
 
